b84cb8d2b70b5021aaf892f0f4671533f5558c6b
wjm <1846253577@qq.com>
2023-05-21 00:09:07 +0800

    第一次提交

---
+++ src/com/wjm/FirstDemo.java
@@ -0,0 +1,13 @@
+package com.wjm;
+
+/**
+ * @Author wang
+ * @Date 2023/5/21  0:07
+ * @Description
+ * @Since version-1.0
+ */
+public class FirstDemo {
+    public static void main(String[] args) {
+
+    }
+}
