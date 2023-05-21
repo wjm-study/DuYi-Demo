0975b1a7fcaee6dc842a1886329238b2b3ed370c
wjm <1846253577@qq.com>
2023-05-21 08:58:37 +0800

    test分支提交

---
+++ src/com/wjm/BranchDemo.java
@@ -0,0 +1,13 @@
+package com.wjm;
+
+/**
+ * @Author wang
+ * @Date 2023/5/21  8:57
+ * @Description
+ * @Since version-1.0
+ */
+public class BranchDemo {
+    public static void main(String[] args) {
+        System.out.println("test Branch");
+    }
+}
