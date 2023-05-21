e0303c59f062d16a264f2cac5a35df57778d12e1
wjm <1846253577@qq.com>
2023-05-21 08:02:22 +0800

    第四次提交

--- src/com/wjm/FirstDemo.java
+++ src/com/wjm/FirstDemo.java
@@ -9,5 +9,6 @@ package com.wjm;
 public class FirstDemo {
     public static void main(String[] args) {
         System.out.println("你好，git！");
+        System.out.println("test change!");
     }
 }
